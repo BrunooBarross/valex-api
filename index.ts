1661ed2c97fe08053ea9392917826e97b8bea6ed
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-07-07 21:41:14 -0300

    feat: first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import chalk from "chalk"; 
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(json());
+
+const port = +process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(chalk.bold.blue("listening on port " + port));
+})
