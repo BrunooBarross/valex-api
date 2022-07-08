cc05df88dff2ca939ab0c2dbea289f589bece3e7
Bruno Barros <barrosbrunofelipe@gmail.com>
2022-07-08 16:18:28 -0300

    feat: adicionando ao card services a função para converter o nome do títular do cartão

--- services/cardsServices.ts
+++ services/cardsServices.ts
@@ -1,9 +1,13 @@
 import * as employeeRepository from "../repositories/employeeRepository.js"
 import * as cardRepository from "../repositories/cardRepository.js"
 import { TransactionTypes } from "../repositories/cardRepository.js"
+import { faker } from '@faker-js/faker';
+import { NotificationResponseMessage } from "pg-protocol/dist/messages.js";
 
 export async function createCard(employeeId: number, type: TransactionTypes) {
     await checkExistingEmployee(employeeId);
+    await checkExistingCardType(employeeId, type);
+    await generateDataCard(employeeId, type);
 }
 
 async function checkExistingEmployee(employeeId: number) {
@@ -18,4 +22,28 @@ async function checkExistingCardType(employeeId: number, type: TransactionTypes)
     if (checkExistingCardType) {
         throw { type: "conflict", message: `the employee already has the ${type} card` }
     }
+}
+
+async function generateDataCard ( employeeId: number, type: TransactionTypes) {
+   const cardNumber = generateCardNumber();
+   const cardName = await generateHolderName(employeeId);
+}
+
+function generateCardNumber() {
+    return faker.finance.creditCardNumber('visa');
+}
+
+async function generateHolderName(employeeId: number) {
+    const employee = await employeeRepository.findById(employeeId);
+    const name = employee.fullName.split(" ");
+    const middle = new Array;
+
+    for (let i = 1; i < name.length - 1; i++) {
+        if (name[i].length > 3) {
+            middle.push(name[i][0]);
+        }
+    }
+
+    const convertedName = [name[0], middle, name[name.length - 1]];
+    return convertedName.join(" ").toUpperCase();
 }
